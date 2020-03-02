d0c2c7631d8454270fa359dccf4cda19b421f5a6
NVasan20 <Nvasan20@cranbrook.edu>
2020-03-01 21:02:57 -0500

    New Article Controller+View

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,22 @@ class ArticlesController < ApplicationController
 	def index 
 		@articles = Article.all
 	end 
+
+	def show
+		id = params[:id]
+		@article = Article.find(id)
+	end
+
+	def recent
+		@articles = Article.all.limit(10).order(created_at: :desc)
+		render :index
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create 
+		@article = Article.create(params.require(:article).permit(:title, :body))
+	end 
 end
